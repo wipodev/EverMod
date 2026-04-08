a5f9573c99dcb253b331daaa02baf70f9dda5ef2
wipodev <ajwipo@gmail.com>
2026-04-08 16:12:43 -0500

    feat(evermod): implement GUI overlay compatibility for Minecraft 1.21 via CustomizeGuiOverlayEvent

--- evermod-1.20.1/src/main/java/net/evermod/client/event/EverGuiEvents.java
+++ evermod-1.20.1/src/main/java/net/evermod/client/event/EverGuiEvents.java
@@ -2,6 +2,7 @@ package net.evermod.client.event;
 
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -9,19 +10,26 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class EverGuiEvents {
 
+  private static final String EVER_ID = "evermod:main_overlay";
+
   @SubscribeEvent
   public static void onVanillaPre(RenderGuiOverlayEvent.Pre event) {
-    EverRenderGuiOverlayEvent.Pre everEvent = new EverRenderGuiOverlayEvent.Pre(
-        event.getGuiGraphics(), event.getPartialTick(), event.getOverlay().id().toString(),
-        event.getGuiGraphics().guiWidth(), event.getGuiGraphics().guiHeight());
-    if (MinecraftForge.EVENT_BUS.post(everEvent))
-      event.setCanceled(true);
+    if (event.getOverlay().id().equals(VanillaGuiOverlay.HOTBAR.id())) {
+      EverRenderGuiOverlayEvent.Pre everEvent =
+          new EverRenderGuiOverlayEvent.Pre(event.getGuiGraphics(), event.getPartialTick(), EVER_ID,
+              event.getGuiGraphics().guiWidth(), event.getGuiGraphics().guiHeight());
+      if (MinecraftForge.EVENT_BUS.post(everEvent)) {
+        event.setCanceled(true);
+      }
+    }
   }
 
   @SubscribeEvent
   public static void onVanillaPost(RenderGuiOverlayEvent.Post event) {
-    MinecraftForge.EVENT_BUS.post(new EverRenderGuiOverlayEvent.Post(event.getGuiGraphics(),
-        event.getPartialTick(), event.getOverlay().id().toString(),
-        event.getGuiGraphics().guiWidth(), event.getGuiGraphics().guiHeight()));
+    if (event.getOverlay().id().equals(VanillaGuiOverlay.HOTBAR.id())) {
+      MinecraftForge.EVENT_BUS
+          .post(new EverRenderGuiOverlayEvent.Post(event.getGuiGraphics(), event.getPartialTick(),
+              EVER_ID, event.getGuiGraphics().guiWidth(), event.getGuiGraphics().guiHeight()));
+    }
   }
 }
--- evermod-1.21/src/main/java/net/evermod/client/event/EverGuiEvents.java
+++ evermod-1.21/src/main/java/net/evermod/client/event/EverGuiEvents.java
@@ -1,7 +1,7 @@
 package net.evermod.client.event;
 
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import net.minecraftforge.client.event.CustomizeGuiOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -9,19 +9,23 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class EverGuiEvents {
 
-  @SubscribeEvent
-  public static void onVanillaPre(RenderGuiOverlayEvent.Pre event) {
-    EverRenderGuiOverlayEvent.Pre everEvent = new EverRenderGuiOverlayEvent.Pre(
-        event.getGuiGraphics(), event.getPartialTick(), event.getOverlay().id().toString(),
-        event.getGuiGraphics().guiWidth(), event.getGuiGraphics().guiHeight());
-    if (MinecraftForge.EVENT_BUS.post(everEvent))
-      event.setCanceled(true);
-  }
+  private static final String EVER_ID = "evermod:main_overlay";
 
   @SubscribeEvent
-  public static void onVanillaPost(RenderGuiOverlayEvent.Post event) {
-    MinecraftForge.EVENT_BUS.post(new EverRenderGuiOverlayEvent.Post(event.getGuiGraphics(),
-        event.getPartialTick(), event.getOverlay().id().toString(),
-        event.getGuiGraphics().guiWidth(), event.getGuiGraphics().guiHeight()));
+  public static void onOverlayEvent(CustomizeGuiOverlayEvent event) {
+
+    float partialTick = event.getPartialTick();
+    int width = event.getGuiGraphics().guiWidth();
+    int height = event.getGuiGraphics().guiHeight();
+
+    // 1. Disparamos el PRE
+    EverRenderGuiOverlayEvent.Pre preEvent = new EverRenderGuiOverlayEvent.Pre(
+        event.getGuiGraphics(), partialTick, EVER_ID, width, height);
+
+    if (!MinecraftForge.EVENT_BUS.post(preEvent)) {
+      // 2. Disparamos el POST
+      MinecraftForge.EVENT_BUS.post(new EverRenderGuiOverlayEvent.Post(event.getGuiGraphics(),
+          partialTick, EVER_ID, width, height));
+    }
   }
 }
